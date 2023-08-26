2310321de3e57cc8ad67b0058e8027a615e74356
Swarnkar, K Divya <k-divya.swarnkar@capgemini.com>
2023-08-26 15:09:44 +0530

    Reactive  initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup,FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+  signupForm=new FormGroup({
+     name:new FormControl(''),
+     age:new FormControl(''),
+     email:new FormControl('')
+   });
+   handleSubmit()
+   {
+    console.log(this.signupForm);
+    console.log(this.signupForm.value);
+   }
 }
