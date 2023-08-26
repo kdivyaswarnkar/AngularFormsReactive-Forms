2164628ad3c9d917298a8b3755935ed06a6f18d0
Swarnkar, K Divya <k-divya.swarnkar@capgemini.com>
2023-08-26 16:40:57 +0530

    Added Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,20 @@ export class AppComponent {
   title = 'reactive-forms';
 
   signupForm=new FormGroup({
-     name:new FormControl(''),
-     age:new FormControl(''),
-     email:new FormControl('')
+     name:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+     age:new FormControl('',[Validators.required,Validators.min(10),Validators.max(50)]),
+    //  email:new FormControl('',[Validators.required, Validators.pattern('our pattern')])
+     email:new FormControl('',[Validators.required, Validators.email]),
+     pass:new FormControl('',[Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')])
    });
    handleSubmit()
    {
     console.log(this.signupForm);
     console.log(this.signupForm.value);
    }
+get f()
+{
+  return this.signupForm.controls
+}
+
 }
